4fbffd4d047259d534e226f1702138f327a998c8
sadums <samjadams4@gmail.com>
2023-04-27 16:23:41 -0600

    pawns displayed to screen

--- assets/js/scripts/script.js
+++ assets/js/scripts/script.js
@@ -13,7 +13,10 @@ class Tile{
         this.rank = rank;
         this.color = color;
         this.element = element;
-        tiles.set(file + rank, this);
+        tiles.set(this.file + this.rank, this);
+        this.clicked = () => {
+            console.log(this.file + String(this.rank));
+        }
     }
 }
 
@@ -23,21 +26,23 @@ const letters = ['a','b','c','d','e','f','g','h'];
 for(let i = 0; i < 64; i++){
     let file = letters[Math.floor(i / 8)];
     let rank = i % 8 + 1;
-    let color = (i + Math.floor(i / 8)) % 2 ? "white" : "black";
+    let color = (i + Math.floor(i / 8)) % 2 ? "white" : "burlywood";
     let element = document.createElement('div');
     element.setAttribute('id', file + rank);
     element.setAttribute('style', `width: ${squareWidth}px; height: ${squareWidth}px; background-color: ${color}`);
     chessBoardElement.prepend(element);
 
-    new Tile(file, rank, color, element);
+    let tile = new Tile(file, rank, color, element);
+    tile.element.addEventListener('click', tile.clicked);
 }
 console.log(tiles);
 
 // Piece Classes
 
 class Piece{
-    constructor(color){
+    constructor(color, element){
         this.color = color;
+        this.element = element;
     }
     capture(){
 
@@ -48,67 +53,93 @@ class Piece{
 }
 
 class King extends Piece{
-    constructor(color){
-        super(color);
-        this.imgPath = () => {
-            if(color === 'white') return './assets/images/wk.png';
-            return './assets/images/bk.png';
-        }
+    constructor(color, element){
+        super(color, element);
+    }
+    imgPath(){
+        if(this.color === 'white') return './assets/images/wk.png';
+        return './assets/images/bk.png';
     }
 }
 
 class Queen extends Piece{
-    constructor(color){
-        super(color);
-        this.imgPath = () => {
-            if(color === 'white') return './assets/images/wq.png';
-            return './assets/images/bq.png';
-        }
+    constructor(color, element){
+        super(color, element);
+    }
+    imgPath(){
+        if(this.color === 'white') return './assets/images/wq.png';
+        return './assets/images/bq.png';
     }
 }
 
 class Rook extends Piece{
-    constructor(color){
-        super(color);
-        this.imgPath = () => {
-            if(color === 'white') return './assets/images/wr.png';
-            return './assets/images/br.png';
-        }
+    constructor(color, element){
+        super(color,element);
+    }
+    imgPath(){
+        if(this.color === 'white') return './assets/images/wr.png';
+        return './assets/images/br.png';
     }
 }
 
 class Bishop extends Piece{
-    constructor(color){
-        super(color);
-        this.imgPath = () => {
-            if(color === 'white') return './assets/images/wb.png';
-            return './assets/images/bb.png';
-        }
+    constructor(color, element){
+        super(color, element);
+    }
+    imgPath(){
+        if(this.color === 'white') return './assets/images/wb.png';
+        return './assets/images/bb.png';
     }
 }
 
 class Knight extends Piece{
-    constructor(color){
-        super(color);
-        this.imgPath = () => {
-            if(color === 'white') return './assets/images/wn.png';
-            return './assets/images/bn.png';
-        }
+    constructor(color, element){
+        super(color, element);
+    }
+    imgPath(){
+        if(this.color === 'white') return './assets/images/wn.png';
+        return './assets/images/bn.png';
     }
 }
 
 class Pawn extends Piece{
-    constructor(color){
-        super(color);
-        this.imgPath = () => {
-            if(color === 'white') return './assets/images/wp.png';
-            return './assets/images/bp.png';
-        }
+    constructor(color, element){
+        super(color, element);
     }
-
+    
+    imgPath(){
+        if(this.color === 'white') return './assets/images/wp.png';
+        return './assets/images/bp.png';
+    }
     promotes(){
 
     }
 }
 
+const whitePieces = [];
+const blackPieces = [];
+const start = function(){
+    // create and place black pawns
+    // create and place white pawns
+    letters.forEach( (file) => {
+        let blackPawnTile = tiles.get(`${file}7`).element;
+        let blackPawnElement = document.createElement('img');
+        let whitePawnTile = tiles.get(`${file}2`).element;
+        let whitePawnElement = document.createElement('img');
+        blackPawnElement.setAttribute('style', 'z-index: 2; display: block; margin-left: auto; margin-right: auto; width: 100%');
+        whitePawnElement.setAttribute('style', 'z-index: 2; display: block; margin-left: auto; margin-right: auto; width: 100%');
+        let blackPawn = new Pawn('black', blackPawnElement);
+        let whitePawn = new Pawn('white', whitePawnElement);
+        blackPawnElement.setAttribute('src', blackPawn.imgPath());
+        whitePawnElement.setAttribute('src', whitePawn.imgPath());
+        blackPawnTile.append(blackPawnElement);
+        whitePawnTile.append(whitePawnElement);
+        whitePieces.push(whitePawn);
+        blackPieces.push(blackPawn);
+    });
+    // create and place black pieces
+    
+    // create and place black pieces
 
+}
+start();
